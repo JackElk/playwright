7f6baef768098cc746cbd10f7ee8f4e18cd6a8ee
losikyevgen <losikyevgen@gmail,com>
2025-10-03 16:01:41 +0300

    test with playwright/Css selectors and xpath

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -2,7 +2,8 @@ import { test, expect } from '@playwright/test';
 
 test.only('has title', async ({ page }) => {
   await page.goto('https://www.youtube.com/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page.getByLabel("Try searching to get started")).toHaveText("Try searching to get started");
+  await expect(page.getByLabel('Try searching to get started')).toHaveText('Try searching to get started');
+  await page.locator('input[name="search_query"]').fill('Playwright');
+  await page.getByTitle('Search').click();
+  await expect(page.locator("//div[@id='chip-shape-container']//button[@role='tab']//div[normalize-space()='All']")).toHaveText('All');
 });
