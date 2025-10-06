e5d71bd0e330808482b5b4b615b67710bc75dda6
losikyevgen <losikyevgen@gmail,com>
2025-10-06 20:55:51 +0300

    update tests with chained locator

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -6,4 +6,6 @@ test.only('has title', async ({ page }) => {
   await page.locator('input[name="search_query"]').fill('Playwright');
   await page.getByTitle('Search').click();
   await expect(page.locator("//div[@id='chip-shape-container']//button[@role='tab']//div[normalize-space()='All']")).toHaveText('All');
+  await page.getByRole('navigation').getByLabel('You').click();
+  await expect(page.locator('.promo-title.style-scope.ytd-background-promo-renderer')).toHaveText('Enjoy your favorite videos');
 });
