cb75bdc748f3212e846e2da37ff007fb4b078113
losikyevgen <losikyevgen@gmail,com>
2025-10-11 14:54:00 +0300

    add new tests with different assertions

--- tests/Assertions.spec.ts
+++ tests/Assertions.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test.describe("Assertions Tests", async () => {
-    test('Checkbox', async ({ page }) => {
+    test('Checkbox check', async ({ page }) => {
         await page.goto("https://rahulshettyacademy.com/AutomationPractice/");
         await page.locator("#checkBoxOption1").check()
         await expect(page.locator("#checkBoxOption1")).toBeChecked();
@@ -15,5 +15,23 @@ test.describe("Assertions Tests", async () => {
     test('Count check', async ({ page }) => {
         await page.goto("https://rahulshettyacademy.com/AutomationPractice/");
         await expect(page.getByRole("radio")).toHaveCount(3)
+    })
+
+    test('Button check', async ({ page }) => {
+        await page.goto("https://rahulshettyacademy.com/AutomationPractice/");
+        await page.locator("#hide-textbox").click()
+        await expect(page.getByPlaceholder("Enter Your Name")).not.toBeHidden()
+    })
+
+    test('Radio button check', async ({ page }) => {
+        await page.goto("https://rahulshettyacademy.com/AutomationPractice/");
+        await page.locator('.radioButton').first().check()
+        await expect(page.locator('.radioButton').first()).toBeChecked();
+    })
+
+    test('Input text check', async ({ page }) => {
+        await page.goto("https://rahulshettyacademy.com/AutomationPractice/");
+        await page.getByPlaceholder("Enter Your Name").fill("test")
+        await expect(page.getByPlaceholder("Enter Your Name")).toHaveValue("test")
     })
 });
