5562325582b6244833d5052fe7cb31684d09d5bd
losikyevgen <losikyevgen@gmail,com>
2025-10-14 12:31:50 +0300

    fixes

--- tests/Assertions.spec.ts
+++ tests/Assertions.spec.ts
@@ -1,32 +1,33 @@
+// @ts-check
 import { test, expect } from './fixtures/base-fixtures.js';
 
 test.describe('Assertions Tests', async () => {
 
-    test('Checkbox check', async ({ page }) => {
-        await page.locator("#checkBoxOption1").check()
-        await expect(page.locator("#checkBoxOption1")).toBeChecked();
+    test('Checkbox check', async ({ academyPage }) => {
+        await academyPage.locator('#checkBoxOption1').check();
+        await expect(academyPage.locator('#checkBoxOption1')).toBeChecked();
+    });
+
+    test('Text check', async ({ academyPage }) => {
+        await expect(academyPage.locator(".totalAmount")).toHaveText("Total Amount Collected: 296")
     })
 
-    test('Text check', async ({ page }) => {
-        await expect(page.locator(".totalAmount")).toHaveText("Total Amount Collected: 296")
+    test('Count check', async ({ academyPage }) => {
+        await expect(academyPage.getByRole("radio")).toHaveCount(3)
     })
 
-    test('Count check', async ({ page }) => {
-        await expect(page.getByRole("radio")).toHaveCount(3)
+    test('Button check', async ({ academyPage }) => {
+        await academyPage.locator("#hide-textbox").click()
+        await expect(academyPage.getByPlaceholder("Enter Your Name")).not.toBeHidden()
     })
 
-    test('Button check', async ({ page }) => {
-        await page.locator("#hide-textbox").click()
-        await expect(page.getByPlaceholder("Enter Your Name")).not.toBeHidden()
+    test('Radio button check', async ({ academyPage }) => {
+        await academyPage.locator(".radioButton").first().check()
+        await expect(academyPage.locator(".radioButton").first()).toBeChecked();
     })
 
-    test('Radio button check', async ({ page }) => {
-        await page.locator(".radioButton").first().check()
-        await expect(page.locator(".radioButton").first()).toBeChecked();
-    })
-
-    test('Input text check', async ({ page }) => {
-        await page.getByPlaceholder("Enter Your Name").fill("test")
-        await expect(page.getByPlaceholder("Enter Your Name")).toHaveValue("test")
+    test('Input text check', async ({ academyPage }) => {
+        await academyPage.getByPlaceholder("Enter Your Name").fill("test")
+        await expect(academyPage.getByPlaceholder("Enter Your Name")).toHaveValue("test")
     })
 });
